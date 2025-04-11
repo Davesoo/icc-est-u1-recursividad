475276840baf93ce6719795f0b5f57352aa977bf
Davesoo <dsiguenzav@est.ups.edu.ec>
2025-04-10 22:04:57 -0500

    Recursividad

--- src/EjerciciosRecursivos.java
+++ src/EjerciciosRecursivos.java
@@ -1,6 +1,5 @@
 public class EjerciciosRecursivos {
 
-    // Método para calcular Fibonacci
     public int fibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -8,7 +7,6 @@ public class EjerciciosRecursivos {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
-    // Método para calcular potencia de un número de manera recursiva
     public int potencia(int base, int exponente) {
         if (exponente == 0) {
             return 1;
@@ -16,18 +14,14 @@ public class EjerciciosRecursivos {
         return base * potencia(base, exponente - 1);
     }
 
-    // Método para calcular la suma de los dígitos de un número
     public static int sumaDigitos(int numero) {
-        // Caso base: si el número es menor que 10, devuelve el mismo número
         if (numero < 10) {
             return numero;
         }
         
-        // Caso recursivo: extraer el último dígito con el operador %
         return (numero % 10) + sumaDigitos(numero / 10);
     }
 
-    // Método para sumar números consecutivos de forma recursiva
     public int sumaConsecutivos(int n) {
         if (n == 1) {
             return 1;
